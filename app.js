a57d4818ed1ae4ee0bdf8a9a6f566cdf4b0456fd
awa760 <138521371+awa760@users.noreply.github.com>
2023-10-04 00:22:32 +0500

    this is react assignment
    this is react assignment

---
+++ app.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <>
+      <div className="card">
+        <div className="card-image">
+        <img src= "https://images.unsplash.com/photo-1521139869420-edaae1bc7b9a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ" alt="abc" />
+        </div>
+        <div className="card-title">
+          <h1>Swat</h1>
+          <p>Swat District, also known as the Swat</p>
+        </div>
+        <div className="card-copy">
+          <p>Swat District, also known as the Swat valley, is a district in the Malakand Division of Khyber Pakhtunkhwa, pakistan, with a population of 2,309,570 per the 2017 national census , Swat is the 15th-largest district of Khyber Pakhtunkhaw with many popular tourist attractions.</p>
+        </div>
+      </div>
+    </>
+  
+  );
+}
+
+export default App;
